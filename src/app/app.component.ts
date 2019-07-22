1dbb256d0b5925f801315fd8db02f15cfdffbe9a
ChuckNezi <jnezia1@gmail.com>
2019-07-22 19:07:42 -0400

    focusing on how classes work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<h1>{{title}}</h1>
-  <h2>{{hero}} Details</h2>`, 
+  <h2>{{hero.name}} Details {{ hero.id }}</h2>
+  <div><label>id: </label>{{hero.id}} </div>
+  <div><label>name: </label>{{hero.name}} </div>
+
+
+  
+  `, 
    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Tour of Heroes'
-  hero = 'Windstorm'
+  hero : Hero = {
+    id : 1,
+    name: 'Windstorm'
+
+  }
+}
+
+export class Hero {
+  id: number
+  name: String
 }
